bbef14adc56b7cbf8b64f0d50a42aceca2558a76
uzuraptor <phuckinganimals@gmail.com>
2020-01-14 01:52:16 +1000

    Второй коммит, закончил миникурс. Оно работает D:

--- src/sql/demo/repository/TableOperations.java
+++ src/sql/demo/repository/TableOperations.java
@@ -2,7 +2,7 @@ package sql.demo.repository;
 
 import java.sql.SQLException;
 
-public interface TableOperation {
+public interface TableOperations {
     void createTable() throws SQLException;
     void createForeignKeys() throws SQLException;
 }
